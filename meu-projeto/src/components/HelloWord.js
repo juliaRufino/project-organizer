5616927386d772835ab9efd62cde573bac153188
Julia Rufino <julia.rufino@sptech.school>
2023-12-11 23:32:31 -0300

    Adicionando os componentes e os props

---
+++ meu-projeto/src/components/HelloWord.js
@@ -0,0 +1,12 @@
+import Frase from "./Frase"
+
+function HelloWord() {
+    return (
+        <div>
+            <Frase/>
+            <h1>Meu primeiro componente!</h1>
+        </div>
+    )
+}
+
+export default HelloWord
